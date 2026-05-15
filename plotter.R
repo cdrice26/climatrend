8f317cd95a8dca6ac1a476842be1fe5fd1a0b877
cdrice26 <caleb.rice.2626@gmail.com>
2026-05-15 15:25:25 -0400

    quality: Updated plot labels

--- plotter.R
+++ plotter.R
@@ -13,11 +13,33 @@ prepare_df <- function(weather_results) {
   as.data.frame(long)
 }
 
+metric_labeller <- function(variable) {
+  lookup <- c(
+    "temperature" = "Average Daily Temperature (°C)",
+    "apparent_temperature" = "Average Daily Apparent Temperature (°C)",
+    "precipitation" = "Daily Precipitation (mm)",
+    "snowfall" = "Daily Snowfall (cm)",
+    "windspeed" = "Daily Max Wind Speed (km/h)"
+  )
+  lookup[variable]
+}
+
 make_plots <- function(long) {
   ggplot(
     data = long,
     mapping = aes(x = date, y = value, color = forecasted)
   ) +
     geom_line() +
-    facet_grid(location ~ variable, scales = "free_y")
+    facet_grid(
+      location ~ variable,
+      scales = "free_y",
+      labeller = labeller(variable = metric_labeller)
+    ) +
+    scale_color_manual(
+      values = c("FALSE" = "#1f77b4", "TRUE" = "#ff7f0e"),
+      breaks = c(FALSE, TRUE),
+      labels = c("Historical", "Forecasted"),
+      guide = guide_legend(title = NULL)
+    ) +
+    labs(x = "Date", y = NULL)
 }
